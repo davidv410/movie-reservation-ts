1435fec7f7e4425b94723b6c405ed7110450014c
davidv410 <davidvrgoc99@gmail.com>
2026-07-30 17:03:36 +0200

    added cache to seats

--- src/services/seats.service.ts
+++ src/services/seats.service.ts
@@ -1,11 +1,19 @@
 import {db} from "../db/db.js";
 import {eq} from "drizzle-orm";
 import {seats} from "../db/schema.js";
+import { redis } from "../lib/redis.js";
 
 export class SeatsService {
     async findSeats (id: string) {
+        const key = `showtimeSeats:${id}`
+
+        const cached = await redis.get(key)
+        if(cached) return cached
+        
         const showtimeSeats = await db.select().from(seats).where(eq(seats.showtimeId, id))
-        if(showtimeSeats.length === 0) return []
+
+        await redis.set(key, JSON.stringify(showtimeSeats), { ex: 60 * 60 })
+
         return showtimeSeats
     }
 }
