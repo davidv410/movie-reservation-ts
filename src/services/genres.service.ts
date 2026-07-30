edb3ef948c230c8a5e8b3a6ec7ecb8d819431f34
davidv410 <davidvrgoc99@gmail.com>
2026-07-30 17:10:39 +0200

    removed cache for seats

--- src/services/genres.service.ts
+++ src/services/genres.service.ts
@@ -7,10 +7,7 @@ export class GenresService {
         const key = 'genres'
 
         const cached = await redis.get(key)
-        if(cached) {
-            console.log('cache hit')
-            return cached
-        }
+        if(cached) return cached
 
         const result = await db.select().from(genres)
         await redis.set(key, JSON.stringify(result), { ex: 60 * 60 })
--- src/services/seats.service.ts
+++ src/services/seats.service.ts
@@ -5,15 +5,7 @@ import { redis } from "../lib/redis.js";
 
 export class SeatsService {
     async findSeats (id: string) {
-        const key = `showtimeSeats:${id}`
-
-        const cached = await redis.get(key)
-        if(cached) return cached
-        
         const showtimeSeats = await db.select().from(seats).where(eq(seats.showtimeId, id))
-
-        await redis.set(key, JSON.stringify(showtimeSeats), { ex: 60 * 60 })
-
         return showtimeSeats
     }
 }
