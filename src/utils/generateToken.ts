31078ca25b1a3ca5a39ae2b912c36c435f1c48ba
davidv410 <davidvrgoc99@gmail.com>
2026-09-13 12:43:18 +0200

    type changes

--- src/utils/generateToken.ts
+++ src/utils/generateToken.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config'
 import jwt from "jsonwebtoken";
 
-export const accessToken = (id: number, role: string, email: string) => { return jwt.sign({ id, role, email }, process.env.ACCESS_TOKEN_SECRET!, { expiresIn: '1h' })}
-export const refreshToken = (id: number, role: string, email: string) => { return jwt.sign({ id, role, email }, process.env.REFRESH_TOKEN_SECRET!, { expiresIn: '7d' }) }
+export const accessToken = (id: string, role: string, email: string) => { return jwt.sign({ id, role, email }, process.env.ACCESS_TOKEN_SECRET!, { expiresIn: '1h' })}
+export const refreshToken = (id: string, role: string, email: string) => { return jwt.sign({ id, role, email }, process.env.REFRESH_TOKEN_SECRET!, { expiresIn: '7d' }) }
