9d352c6fdbdb8e17b64d3b27a6d04cc56df97503
davidv410 <davidvrgoc99@gmail.com>
2026-09-16 02:41:58 +0200

    added function for date formatting

---
+++ src/utils/formatDate.ts
@@ -0,0 +1,9 @@
+export const formatDateTime = (date: string | Date) => {
+    return new Date(date).toLocaleString('en-GB', {
+        day: '2-digit',
+        month: 'short',
+        year: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+    })
+}
