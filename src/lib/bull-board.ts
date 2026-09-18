29e960897262c7bc8cfb288093c6a359854e885c
davidv410 <davidvrgoc99@gmail.com>
2026-09-18 21:22:55 +0200

    completeReservationQueue added to bull-board

--- src/lib/bull-board.ts
+++ src/lib/bull-board.ts
@@ -3,12 +3,13 @@ import { BullMQAdapter } from "@bull-board/api/bullMQAdapter";
 import { ExpressAdapter } from "@bull-board/express";
 import { emailQueue } from "../services/queues/email.queue.js";
 import { cleanupQueue } from "../services/queues/cleanup.queue.js";
+import { completeReservationQueue } from "../services/queues/complete-reservations.queue.js";
 
 const serverAdapter = new ExpressAdapter();
 serverAdapter.setBasePath("/admin/queues");
 
 createBullBoard({
-    queues: [new BullMQAdapter(emailQueue), new BullMQAdapter(cleanupQueue)],
+    queues: [new BullMQAdapter(emailQueue), new BullMQAdapter(cleanupQueue), new BullMQAdapter(completeReservationQueue)],
     serverAdapter,
 });
 
