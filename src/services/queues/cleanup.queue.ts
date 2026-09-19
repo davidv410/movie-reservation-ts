b9d056fcbb876229a4eea79b5f3014a2a0ce90d1
davidv410 <davidvrgoc99@gmail.com>
2026-09-19 18:52:33 +0200

    queue options update

--- src/services/queues/cleanup.queue.ts
+++ src/services/queues/cleanup.queue.ts
@@ -5,6 +5,8 @@ export const cleanupQueue = new Queue("cleanup", {
     connection: queueConnection,
     defaultJobOptions: {
         attempts: 3,
-        backoff: { type: "exponential", delay: 2000 }
+        backoff: { type: "exponential", delay: 10_000 },
+        removeOnComplete: { age: 3600, count: 100 },
+        removeOnFail: { age: 86400, count: 100 },
     }
 })
--- src/services/queues/email.queue.ts
+++ src/services/queues/email.queue.ts
@@ -6,6 +6,8 @@ export const emailQueue = new Queue<EmailJob>("email", {
     connection: queueConnection,
     defaultJobOptions: {
         attempts: 3,
-        backoff: { type: "exponential", delay: 2000 }
+        backoff: { type: "exponential", delay: 10_000 },
+        removeOnComplete: { age: 3600, count: 100 },
+        removeOnFail: { age: 86400, count: 100 },
     }
 })
