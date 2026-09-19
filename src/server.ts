081635247eaedb6bc20f53958d00db2ea15ff5f4
davidv410 <davidvrgoc99@gmail.com>
2026-09-19 18:53:56 +0200

    commented out workers until limit reset

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config'
-import './services/queues/email.worker.js'
-import './services/queues/cleanup.worker.js'
-import './services/queues/complete-reservations.worker.js'
+import './services/complete-reservation.js'
+// import './services/queues/email.worker.js'
+// import './services/queues/cleanup.worker.js'
 import app from "./app.js";
 
 const PORT = process.env.PORT || 5000;
